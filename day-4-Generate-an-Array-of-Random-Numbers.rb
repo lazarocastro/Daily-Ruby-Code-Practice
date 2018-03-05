b471229db9de930ccc6ca3a6037b5b711ec1e448
Lazaro A. S. Castro <lazarocastro@gmail.com>
2018-03-05 11:22:25 -0300

    Fix exercice day 4

--- day-4-Generate-an-Array-of-Random-Numbers.rb
+++ day-4-Generate-an-Array-of-Random-Numbers.rb
@@ -1,14 +1,15 @@
 require 'rspec'
 
 def random_numbers
-  Array.new(20) { rand 1000 }
+  Array.new(1000) { rand 1000 }
 end
+p random_numbers.count
 
 describe 'Random number collection generator' do
-  it 'creates a collection of random numbers rangin drom 1 to 1000' do
+  it 'creates a collection of random numbers rangin from 1 to 1000' do
     random_set_one = random_numbers
     random_set_two = random_numbers
-    expect(random_numbers.count).to eq(20)
+    expect(random_numbers.count).to eq(1000)
     expect(random_set_one).to_not eq(random_set_two)
   end
 end
