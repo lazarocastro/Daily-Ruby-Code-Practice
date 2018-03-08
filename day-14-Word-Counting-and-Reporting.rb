926bd51d6010924c96f7823b9a9b013e5f968814
Lazaro A. S. Castro <lazarocastro@gmail.com>
2018-03-08 12:38:38 -0300

    Day 14 Word Counting and Reporting

---
+++ day-14-Word-Counting-and-Reporting.rb
@@ -0,0 +1,41 @@
+require 'rspec'
+
+class String
+  def total_words
+    scan(/\w+/).count
+  end
+
+  def word_list
+    list = Hash.new(0)
+    downcase.scan(/\w+/) { |w| list[w] += 1 }
+    list
+  end
+end
+
+# uncomment if you want to test with "seeing_is_believing"
+# str = "- the quick brow fox / jumped over the lazy fox."
+# str.total_words
+# str.word_list
+
+
+describe 'Word Reporter' do
+  before do
+    @str = "- the quick brown fox / jumped over the lazy fox."
+  end
+  
+  it 'Counts words accurately' do
+    expect(@str.total_words).to eq(9)
+  end
+
+  it 'Returns a hash that totals up word usage' do
+    expect(@str.word_list).to eq({
+      "the"=>2,
+      "quick"=>1,
+      "brown"=>1,
+      "fox"=>2,
+      "jumped"=>1,
+      "over"=>1,
+      "lazy"=>1
+    })
+  end
+end
