0f3ce72b2dc9d74064e53f60c13e7d84ecb4099f
Lazaro A. S. Castro <lazarocastro@gmail.com>
2018-03-13 16:13:16 -0300

    Day 19 Calculating Large Decimals

---
+++ day-19-Accurately-Calculating-Large-Decimals.rb
@@ -0,0 +1,26 @@
+require 'rspec'
+require 'bigdecimal' # We need to call "bigdecimal" library
+
+def large_number_processor num_1, num_2
+  (BigDecimal(num_1) * BigDecimal(num_2)).to_s('F')
+end
+
+# This is the wrong way
+val_1 = 10.0987345897234093284023
+val_2 = 42.0987345897234093284023
+val_1 * val_2
+
+# This the correct method
+val_1 = "10.0987345897234093284023"
+val_2 = "42.0987345897234093284023"
+large_number_processor val_1, val_2
+
+
+describe 'Large number processing' do
+  it 'accurately multiplies 2 large numbers' do
+    val_1 = "10.0987345897234093284023"
+    val_2 = "42.0987345897234093284023"
+    expect(large_number_processor(val_1, val_2)).to eq("425.14394718482513504388005529072886672947064529")
+  end
+end
+
