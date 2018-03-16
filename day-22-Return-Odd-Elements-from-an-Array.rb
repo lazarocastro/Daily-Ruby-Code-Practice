e123e38d4e49928e3e2c1a58ade4ea3667ebec82
Lazaro A. S. Castro <lazarocastro@gmail.com>
2018-03-16 17:56:14 -0300

    Day 22 Return Odd Elements from an Array.

---
+++ day-22-Return-Odd-Elements-from-an-Array.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+
+class Array
+  def odd_selector
+    select(&:odd?)
+  end
+end
+
+describe 'Odd selector' do
+  it 'given an array, it  returns a collection of the odd elements' do
+    expect(Array(1..10).odd_selector).to eq([1, 3, 5, 7, 9])
+  end
+end
+
+arr = Array(1..10)
+arr.odd_selector
+
