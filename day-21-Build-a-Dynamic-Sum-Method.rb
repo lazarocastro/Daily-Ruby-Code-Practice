f7f73db446e75e276b71a55f7eb1aa1601e6ae1a
Lazaro A. S. Castro <lazarocastro@gmail.com>
2018-03-15 22:36:23 -0300

    Day 21 Build a Dynamic Sum Method

---
+++ day-21-Build-a-Dynamic-Sum-Method.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+
+def sum_generator num
+  1.upto(num).inject(&:+)
+end
+
+sum_generator(10)
+
+describe 'Dynamic sum' do
+  it 'Outputs a sum of digits, ranging from 0 through the argument value' do
+    expect(sum_generator 10).to eq(55)
+  end
+end
