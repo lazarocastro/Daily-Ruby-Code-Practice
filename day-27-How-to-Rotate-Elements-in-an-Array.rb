339ef0b2e079b8a7d35d855a5b4ada0fc111c008
Lazaro A. S. Castro <lazarocastro@gmail.com>
2018-03-21 14:09:40 -0300

    Day 27 How to Rotate Elements in an Array

---
+++ day-27-How-to-Rotate-Elements-in-an-Array.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+
+def array_rotation arr, num
+  arr.rotate(num)
+end
+array_rotation([1, 2, 3], 2)
+[1, 2, 3].rotate(2)
+
+describe 'Array index change' do
+  it 'Rotates the index values for each for the array elements' do
+    expect(array_rotation([1, 2, 3], 2)).to eq([3, 1, 2])
+  end
+end
